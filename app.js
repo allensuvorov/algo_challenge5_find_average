22b03c3fd4d22c3f02e787cd3d5fee57f7f70647
Allen <allenvs@mail.ru>
2020-02-17 01:05:29 +0300

    wrapped code in to function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var arr = [1,3,5,7,20]; // answer: average is 7.2
-var sum = 0;
-for (let i=0;i<arr.length;i++) sum +=arr[i];
-console.log(sum, sum/arr.length);
+average = (arr) => {
+
+    var sum = 0;
+    for (let i=0;i<arr.length;i++) sum +=arr[i];
+    console.log(sum, sum/arr.length);
+};
+average(arr);
